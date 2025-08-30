b581068e3464259864f2e21ae3c3b68f2a8e497f
AnnSeliger <annaseliger@yandex.ru>
2025-08-30 15:39:53 +0300

    Updates

---
+++ assets/all-wallets-DWt0gNdG.js
@@ -0,0 +1,6 @@
+import{b as l}from"./core-1PRETCys.js";import"./index-BFEGyaRh.js";import"./index.es-BbiMQm5N.js";const i=l`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{i as allWalletsSvg};
---
+++ assets/cursor-CN_VaXiQ.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-1PRETCys.js";import"./index-BFEGyaRh.js";import"./index.es-BbiMQm5N.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
