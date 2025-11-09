774fd3f55e7eae3f8b1a835106af61ddfcc1b5ad
AnnSeliger <annaseliger@yandex.ru>
2025-11-09 12:45:36 +0300

    Update production build with new features

--- dist/assets/chevron-top-CxMLiZOX.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-8gY3fVoZ.js";import"./index-Nh7XgN-c.js";import"./index.es-C-1U4X1w.js";const i=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M14.54 11.04a1 1 0 0 1-1.41 0L8 5.92l-5.13 5.12a1 1 0 1 1-1.41-1.41l5.83-5.84a1 1 0 0 1 1.42 0l5.83 5.84a1 1 0 0 1 0 1.41Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{i as chevronTopSvg};
---
+++ dist/assets/cursor-BXMfdRYZ.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-cRSYNy-f.js";import"./index-C4x7BQB0.js";import"./index.es-BCMXxVy8.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
