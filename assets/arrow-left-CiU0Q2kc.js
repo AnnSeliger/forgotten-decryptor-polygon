8cbc5ac1d14f2ed52e2b33a144099cbd6e3295b0
AnnSeliger <annaseliger@yandex.ru>
2025-11-10 07:41:44 +0300

    Updates

---
+++ assets/arrow-left-CiU0Q2kc.js
@@ -0,0 +1,8 @@
+import{b as o}from"./core-wJK3HHhF.js";import"./main-DlMp4bUg.js";import"./index.es-BR6UTWco.js";const i=o`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M13 7.99a1 1 0 0 1-1 1H4.4l2.46 2.46a1 1 0 1 1-1.41 1.41L1.29 8.7a1 1 0 0 1 0-1.41L5.46 3.1a1 1 0 0 1 1.41 1.42L4.41 6.99H12a1 1 0 0 1 1 1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{i as arrowLeftSvg};
---
+++ assets/cursor-CfGimdhh.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-wJK3HHhF.js";import"./main-DlMp4bUg.js";import"./index.es-BR6UTWco.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
